de2aff5c77541376bf32e72ea235d3fb7dbba332
Emma Lindsay <emma@flickme.com>
2014-11-17 22:31:56 -0800

    Fix second hand for clock 3

--- app/assets/javascripts/clock1.js
+++ app/assets/javascripts/clock1.js
@@ -17,7 +17,7 @@ function setTime() {
   $(".smooth_hour_line").css('transform', 'rotate('+((hours_12)*3600 + minutes*60 + seconds)*360/43200+'deg)');
   $(".smooth_24_hour_line").css('transform', 'rotate('+((hours)*3600 + minutes*60 + seconds)*360/86400+'deg)');
   $(".minute_line").css('transform', 'rotate('+minutes*6+'deg)');
-  $(".minute_line").css('transform', 'rotate('+minutes*6+'deg)');
+  $(".second_line").css('transform', 'rotate('+seconds*6+'deg)');
   $(".rotating_clock").css('transform', 'rotate('+ -1*((hours_12)*3600 + minutes*60 + seconds)*360/43200+'deg)');
 }
 setInterval(setTime,1000);
